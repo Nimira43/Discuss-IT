0504cc02486ca6f6df7c97071ccffc4af37cf148
Nick Rabone <n.rabone72@aol.com>
2025-01-12 09:40:43 +0000

    Tidied up styling - better.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from '@nextui-org/react'
 export default function Home() {
   return (
     <div className='bg-[#012222] p-10'>
-      <h1 className='logo text-3xl text-[#ff4500] tracking-wider'>Discuss IT</h1>
-      <hr />
-      <Button>Find Out More</Button>
+      <h1 className='logo text-3xl text-[#ff4500] tracking-wider pb-4'>Discuss IT</h1>
+     
+      <Button className='bg-[#ff4500]'>Find Out More</Button>
     </div>
   )
 }
