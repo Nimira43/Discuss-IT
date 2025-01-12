4719eb8d862d2a9200d641928fcdc4a7c303c2cb
Nick Rabone <n.rabone72@aol.com>
2025-01-12 09:32:32 +0000

    Brought Button component in from Next UI and tested it and Tailwind - okay.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-const HomePage = () => {
+import { Button } from '@nextui-org/react'
+
+export default function Home() {
   return (
-    <div>
-      <h1 className='logo'>Discuss IT</h1>      
+    <div className='bg-[#012222] p-10'>
+      <h1 className='logo text-3xl text-[#ff4500] tracking-wider'>Discuss IT</h1>
+      <hr />
+      <Button>Find Out More</Button>
     </div>
   )
 }
-
-export default HomePage
 
 
