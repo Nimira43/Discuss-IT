b5641fad034b13dab86b00f5c97e84a3886205f7
Nick Rabone <n.rabone72@aol.com>
2025-01-14 08:54:57 +0000

    Setting up authentication using NextAuth with Prisma and GitHub as providers. PrismaAdapter will connect to the database. GitHub is the authentication provider and added user's ID to the session object.

--- src/auth.ts
+++ src/auth.ts
@@ -10,4 +10,20 @@ if (!GITHUB_CLIENT_ID || GITHUB_CLIENT_SECRET) {
   throw new Error('Missing GitHub OAuth credentials.')
 }
 
-
+NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id
+      }
+      return session
+    }
+  }
+})
